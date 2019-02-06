77de5cd7bdd2a6383c454de5258457d60e330f0e
Shimu <eng.shimu@gmail.com>
2019-02-06 18:52:13 +0600

    secend commits

--- WindowsFormsApplication1/BookShopUseListUi.cs
+++ WindowsFormsApplication1/BookShopUseListUi.cs
@@ -71,40 +71,19 @@ namespace WindowsFormsApplication1
         List<string> quantity = new List<string>();
         List<double> totalPrices = new List<double>();
 
-       // string[] coustomerName = new string[10];
-        //string[] contactNo = new string[10];
-        //string[] address = new string[10];
-       // string[] order = new string[10];
-       // string[] quantity = new string[10];
-               //string[] totalPrices = new string[10];
-        //double[] totalPrices = new double[10];
-
         int i = 0;
         private void DisplayAllButton_Click(object sender, EventArgs e)
         {
-
-
-            /* coustomerName[i] = coustomerNameTextBox.Text;
-             contactNo[i] = contactNoTextBox.Text;
-             address[i] = addressTextBox.Text;
-             order[i] = orderComboBox.Text;
-             quantity[i] = quantityTextBox.Text;*/
-            //int priceTemp;
-            //totalPrices.Add(totalPriceLabel.Text);
-            //totalPrices[i] = totalPriceLabel.Text;
-
-            // int qty = Convert.ToInt32(quantityTextBox.Text);
-            // for (int a = i; a == i; a--)
+            try
             {
+  
                 if (coustomerName.Contains(coustomerNameTextBox.Text) || contactNoTextBox.Text.Length != 11)
                 {
                     if (coustomerName.Contains(coustomerNameTextBox.Text))
-                        MessageBox.Show(" This Coustomer name is already Exist");
+                        MessageBox.Show(" This Customer name is already Exist");
                     else
-                        MessageBox.Show("Please Enter 11 Digits");
+                        MessageBox.Show("Please Enter 11 Digits Numbers ");
                 }
-
-
 
                 else
                 {
@@ -113,54 +92,51 @@ namespace WindowsFormsApplication1
                     address.Add(addressTextBox.Text);
                     order.Add(orderComboBox.Text);
                     quantity.Add(quantityTextBox.Text);
+
                     for (int a = i; a == i; a--)
-                    { 
-                        //int qty = Convert.ToInt32(quantity);
-                        //int qty = Convert.ToInt32(quantityTextBox.Text);
+                    {
+                       
                         if (orderComboBox.Text == "Math")
                         {
                             totalPrices.Add(Convert.ToInt32(quantityTextBox.Text) * 120);
-                            //totalPrices = qty * 120;
-                            //priceTemp = Convert.ToInt32(quantityTextBox.Text) * 120;
+                          
                         }
                         else if (orderComboBox.Text == "English")
                         {
                             totalPrices.Add(Convert.ToInt32(quantityTextBox.Text) * 100);
-                            // totalPrices = qty * 100;
-                            // priceTemp = Convert.ToInt32(quantityTextBox.Text) * 100;
+
                         }
                         else if (orderComboBox.Text == "Bangla")
                         {
                             totalPrices.Add(Convert.ToInt32(quantityTextBox.Text) * 90);
-                            // totalPrices[i] = qty * 90;
-                            // priceTemp = Convert.ToInt32(quantityTextBox.Text) * 90;
+                            
                         }
                         else if (orderComboBox.Text == "Art")
                         {
                             totalPrices.Add(Convert.ToInt32(quantityTextBox.Text) * 80);
-                            // totalPrices[i] = qty * 80;
-                            //priceTemp = Convert.ToInt32(quantityTextBox.Text) * 80;
+                            
                         }
                         else
                             totalPrices.Add(Convert.ToInt32(""));
 
-                    // richTextBox.Text = "";
-                    //for (int j = 0; j < i; j++)
-                    // priceTemp = 0;
-                    // totalPrices[a] = Convert.ToString(priceTemp);
-
-                    richTextBox.Text = richTextBox.Text + " \n " + "\t\t\tCustomer Information" +
-                  " " +
-                  "\n\n\tCoustomer Name: " + coustomerName[a] +
-                  "\n " + "\n\tContact No: " + contactNo[a] +
-                  "\n " + "\n\tAddress: " + address[a] +
-                  "\n " + "\n\tOrder: " + order[a] +
-                  "\n " + "\n\tQuantity: " + quantity[a] +
-                  "\n " + "\n\tTotal Price: " + totalPrices[a];
+                        richTextBox.Text = richTextBox.Text + " \n " + "\t\t\tCustomer Information" +
+                      " " +
+                      "\n\n\tCoustomer Name: " + coustomerName[a] +
+                      "\n " + "\n\tContact No: " + contactNo[a] +
+                      "\n " + "\n\tAddress: " + address[a] +
+                      "\n " + "\n\tOrder: " + order[a] +
+                      "\n " + "\n\tQuantity: " + quantity[a] +
+                      "\n " + "\n\tTotal Price: " + totalPrices[a];
                     }
                 }
                 i++;
             }
+            catch (Exception exepotion)
+            {
+                MessageBox.Show(exepotion.Message);
+            }
+
+            }
         }
     }
-}
+
